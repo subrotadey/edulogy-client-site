eafc9b08943773b300183729aadb860da2ef1e70
subrotadey <subrotadey540@gmail.com>
2022-12-20 03:09:23 +0600

    project initialize and navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/Pages/About/About';
+import Courses from './components/Pages/Courses/Courses';
+import Home from './components/Pages/Home/Home';
+import Navbar from './components/Pages/Shared/Navbar';
+import Teachers from './components/Pages/Teachers/Teachers';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/courses' element={<Courses></Courses>}></Route>
+        <Route path='/teachers' element={<Teachers></Teachers>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Pages/Teachers/Teachers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Teachers = () => {
+    return (
+        <div>
+            <h2>This Is Teachers page</h2>
+        </div>
+    );
+};
+
+export default Teachers;
